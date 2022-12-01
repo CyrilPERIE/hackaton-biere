3306fa13620cd1e5e7267f3d5bacec45f7af083b
unknown <cyril.perie96@gmail.com>
2022-12-01 14:10:42 +0100

    Petite surprise pour les copains

--- frontend/hackaton-biere/src/App.js
+++ frontend/hackaton-biere/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style = {{display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    height: '100vh',}}>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
+      <p style = {{textAlign: "center", color: "#f0e62b"}}>COUCOU LES COPINOUS CA VA ETRE TROP DROLE !!!</p>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
+      <img className = "App-logo" src="https://img.freepik.com/vecteurs-premium/journee-internationale-biere-affiche-flyer-banniere_212474-127.jpg?w=2000"/>
     </div>
   );
 }
